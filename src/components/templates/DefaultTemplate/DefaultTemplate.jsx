6b2d12508427d5e39223a93033d66ebefb6aaa57
Timchik-IT <timur.minullin535@gmail.com>
2024-04-07 20:55:41 +0300

    add scroll to top button

--- src/components/templates/DefaultTemplate/DefaultTemplate.jsx
+++ src/components/templates/DefaultTemplate/DefaultTemplate.jsx
@@ -1,15 +1,40 @@
+import { React, useState, useEffect } from 'react';
+import Button from 'src/components/atoms/Button';
 import Footer from 'src/components/atoms/Footer';
 import Header from '../../atoms/Header';
-import { Wrapper } from './styled';
+import { ButtonWrapper, Wrapper } from './styled';
 
-const DefaultTempla return (
-    <Wrapper>
+const DefaultTemplate = ( {children} ) => { 
+  const [isButtonVisible, setIsButtonVisible] = useState(false);
+
+  const checkScrollPosition = () => {
+    setIsButtonVisible(window.scrollY >= 200);
+  }
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkScrollPosition);
+
+    return () => {
+      window.removeEventListener('scroll', checkScrollPosition);
+    };
+  }, []);
+
+  return (
+    <>
       <Header />
 
-      {children}
+      <Wrapper>{children}</Wrapper>
 
-      <Footer />
-    </Wrapper>
+      {isButtonVisible && (
+        <ButtonWrapper> <Button variant="info" onClick={scrollToTop} lable="^"/> </ButtonWrapper>
+      )}
+
+      <Footer/>
+    </>
   )
 }
 
